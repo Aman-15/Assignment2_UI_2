4415e4c3739617bf607d04602c583f9733d0f16a
Aman Agarwal <amanagarwal1501@gmail.com>
2018-10-01 18:22:32 +0530

    Alert Dialog Box for coffee limit added

--- app/src/main/java/com/example/amanagarwal/assignment2_ui_2/CoffeeAdaptor.java
+++ app/src/main/java/com/example/amanagarwal/assignment2_ui_2/CoffeeAdaptor.java
@@ -1,13 +1,17 @@
 package com.example.amanagarwal.assignment2_ui_2;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,13 +33,13 @@ public class CoffeeAdaptor extends RecyclerView.Adapter<CoffeeAdaptor.CoffeeView
     public CoffeeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.coffee_list, null);
-
         return new CoffeeViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CoffeeViewHolder coffeeViewHolder, int i) {
         Coffee coffee = coffeeList.get(i);
+
 
         if (!selectionState.get(i, false)) {
             coffeeViewHolder.checkBox.setChecked(false);}
@@ -63,7 +67,6 @@ public class CoffeeAdaptor extends RecyclerView.Adapter<CoffeeAdaptor.CoffeeView
             coffeeType = itemView.findViewById(R.id.coffeeType);
             ingredients = itemView.findViewById(R.id.coffeeIngredients);
             checkBox = itemView.findViewById(R.id.checkbox);
-
             checkBox.setOnClickListener(this);
         }
 
@@ -71,8 +74,25 @@ public class CoffeeAdaptor extends RecyclerView.Adapter<CoffeeAdaptor.CoffeeView
         public void onClick(View v) {
             int adapterPosition = getAdapterPosition();
             if (!selectionState.get(adapterPosition, false)) {
-                checkBox.setChecked(true);
-                selectionState.put(adapterPosition, true);
+
+                if (selectionState.size() < 7) {
+                    checkBox.setChecked(true);
+                    selectionState.put(adapterPosition, true);
+                }
+                else {
+                    checkBox.setChecked(false);
+                    AlertDialog.Builder builder;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+                    } else {
+                        builder = new AlertDialog.Builder(context);
+                    }
+                    builder.setTitle("Limit Exceeded!")
+                            .setMessage("You can select atmost 7 coffee types.")
+                            .setPositiveButton(android.R.string.yes, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
             }
             else  {
                 checkBox.setChecked(false);
